5ce91f7d2e04be0695db0ef9ba2a5b47a3b948dd
JasinskiKacper <kacperjasinski78@gmail.com>
2026-02-25 16:28:08 +0100

    added closed form algorithm

---
+++ 01_linear_regression/linear_regression.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+class Linear_Regression:
+    def __init__(self):
+        self.weights = None
+        self.bias = None
+
+    def fit_closed_form(self, X: np.ndarray, 
+                        y: np.ndarray
+                        ) -> None:
+        
+        if not isinstance(X, np.ndarray) or not isinstance(y, np.ndarray):
+            return None
+        
+        if X.ndim == 1: # changing shape from (n, ) to (n, 1)
+            X = np.reshape(X, (X.shape[0], 1))
+        
+        X = np.c_[np.ones(X.shape[0]), X]
+        
+        # w = (X^T @ X)^-1 @ X^T @ y 
+        # solving Ax = b for stability
+        A = X.T @ X
+        b = X.T @ y
+        x = np.linalg.solve(A, b)
+        
+        self.bias = x[0]
+        self.weights = x[1:]
+

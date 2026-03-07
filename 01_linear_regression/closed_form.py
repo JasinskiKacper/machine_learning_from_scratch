add metrics fix and end closed form and gradient descent

01_linear_regression/closed_form.py
@@ -6,20 +6,32 @@ class Linear_Regression_Closed_Form(Linear_Regression):
     def fit(self, 
             X: np.ndarray, 
             y: np.ndarray) -> None:
-        
+        '''
+        Fit linear regression using the closed-form solution.
+        This method computes the optimal weights and bias using the formula:
+            (X^T @ X)w = X^T @ y
+        and solves for the linear model:
+            y_pred = X @ w + b
+
+        Parameters
+        ----------
+        X : np.ndarray of shape (n_samples, n_features)
+        y : np.ndarray of shape (n_samples, )
+        '''
         if not isinstance(X, np.ndarray) or not isinstance(y, np.ndarray):
-            return None
+            raise TypeError
         
         if X.ndim == 1: # changing shape from (n, ) to (n, 1)
             X = np.reshape(X, (X.shape[0], 1))
         
-        X = np.c_[np.ones(X.shape[0]), X]
+        # Add bias (column of ones)
+        X_bias = np.c_[np.ones(X.shape[0]), X]
         
-        # w = (X^T @ X)^-1 @ X^T @ y 
-        # solving Ax = b for stability
-        A = X.T @ X
-        b = X.T @ y
-        x = np.linalg.solve(A, b)
+        # Closed form solution using above formula
+        A = X_bias.T @ X_bias
+        b = X_bias.T @ y
+        result = np.linalg.solve(A, b)
         
-        self.bias = x[0]
-        self.weights = x[1:]
+        # Extract bias and weights
+        self.bias = result[0]
+        self.weights = result[1:]

01_linear_regression/gradient_descent.py
@@ -1,5 +1,6 @@
-from linear_regression import Linear_Regression
 import numpy as np
+from linear_regression import Linear_Regression, RMSE
+
 
 class Linear_Regression_Gradient(Linear_Regression):
 
@@ -7,24 +8,51 @@ class Linear_Regression_Gradient(Linear_Regression):
             X: np.ndarray, 
             y: np.ndarray,
             max_iter: int,
-            lr: float) -> None:
+            lr: float,
+            stats: bool = False) -> None:
         '''
-        Args:
-            X (np.ndarray): Data 
-            y (np.ndarray): Data Labels
-            max_iter (int):
-            lr (int): 
+        Fit linear regression using the gradient descent method by
+        calculating predictions, error, and derivatives of weights and bias,
+        starting from random values and subtracting the error multiplied
+        by the learning rate in every iteration.
+
+        Parameters
+        ----------
+        X : np.ndarray of shape (n_samples, n_features)
+        y : np.ndarray of shape (n_samples, )
+        max_iter : int number of iterations
+        lr : int controlling the step size of weights and bias updates
+        stats : bool if True prints RMSE every iteration
         '''
         if not isinstance(X, np.ndarray) or not isinstance(y, np.ndarray):
-            return None
+            raise TypeError
         
+        # Changing shape from (n, ) to (n, 1)
         if X.ndim == 1:
-            X = np.reshape(X, (X.shape[0], 1))
+            X = np.reshape(X, (-1, 1))
+        if y.ndim == 1:
+            y = np.reshape(y, (-1, 1))
 
-        w = np.random.rand(X.shape[0], X.shape[1])
+        # Initialize weights and bias with small random values
+        w = np.random.rand(X.shape[1], 1)
         b = np.random.uniform(-0.01, 0.01)
+        n = y.size
 
-        for iter in range(max_iter):
-            y_approx = X @ w + b
-            error = y_approx - y
+        for _ in range(max_iter):
+            y_pred = X @ w + b
+
+            error = y_pred - y
+
+            if stats:
+                print(f'RMSE: {RMSE(y, y_pred)}')
             
+            # Compute gradients of the loss function
+            dw = (2 / n) * X.T @ (error)
+            db = (2 / n) * np.sum(error)
+
+            # Updating weights and bias
+            w = w - (lr * dw)
+            b = b - (lr * db)
+        
+        self.weights = w
+        self.bias = b